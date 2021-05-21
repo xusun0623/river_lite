4ff83f4dac67a8f971ec9646eb70c9d1612e934a
xusun000 <2235861811@qq.com>
2021-05-21 19:28:05 +0800

    更新跨域解决方案

---
+++ lib/server/cors.dart
@@ -0,0 +1,24 @@
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_proxy/shelf_proxy.dart';
+
+//前端页面访问本地域名
+const String LocalHost = 'localhost';
+
+//前端页面访问本地端口号
+const int LocalPort = 4040;
+
+//目标域名，这里我们将要请求火狐的这个地址
+const String TargetUrl = 'https://www.ioffershow.com';
+
+Future main() async {
+  var server = await shelf_io.serve(
+    proxyHandler(TargetUrl),
+    LocalHost,
+    LocalPort,
+  );
+  // 添加上跨域的这几个header
+  server.defaultResponseHeaders.add('Access-Control-Allow-Origin', '*');
+  server.defaultResponseHeaders.add('Access-Control-Allow-Credentials', true);
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
