870fd6ff320493b8537fd42d161a87560784f003
xusun0623 <45837271+xusun0623@users.noreply.github.com>
2023-01-21 23:20:03 +0800

    update: 所有用户默认自动答题

--- lib/page/new_mix/new_mix.dart
+++ lib/page/new_mix/new_mix.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:offer_show/asset/color.dart';
 import 'package:offer_show/asset/home_desktop_mode.dart';
 import 'package:offer_show/components/newNaviBar.dart';
+import 'package:offer_show/page/topic/Your_emoji.dart';
 import 'package:offer_show/util/provider.dart';
 import 'package:provider/provider.dart';
+
+final GlobalKey<MixContSectionState> mixContSectionKey = GlobalKey();
 
 class PostNewMix extends StatefulWidget {
   int board_id;
@@ -18,6 +25,8 @@ class PostNewMix extends StatefulWidget {
 }
 
 class _PostNewMixState extends State<PostNewMix> {
+  bool choosingEmoji = false;
+
   @override
   Widget build(BuildContext context) {
     return Baaaar(
@@ -44,18 +53,96 @@ class _PostNewMixState extends State<PostNewMix> {
             },
           ),
         ),
-        body: Container(
-          color: Provider.of<ColorProvider>(context).isDark
-              ? os_dark_back
-              : os_white,
-          child: Stack(
-            children: [
-              ListView(
-                children: [
-                  MixTitileInput(),
-                ],
-              ),
-            ],
+        body: KeyboardVisibilityBuilder(
+          builder: (p0, isKeyboardVisible) => Container(
+            color: Provider.of<ColorProvider>(context).isDark
+                ? os_dark_back
+                : os_white,
+            child: Stack(
+              children: [
+                ListView(
+                  children: [
+                    MixTitleInput(),
+                    MixContSection(key: mixContSectionKey),
+                    Container(
+                      height: (!isKeyboardVisible && choosingEmoji) ? 360 : 70,
+                    ),
+                  ],
+                ),
+                // isKeyboardVisible
+                true || choosingEmoji
+                    ? Positioned(
+                        child: Container(
+                          color: os_white,
+                          height:
+                              (!isKeyboardVisible && choosingEmoji) ? 360 : 70,
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: os_grey,
+                                    ),
+                                  ),
+                                ),
+                                width: MediaQuery.of(context).size.width,
+                                child: ResponsiveWidget(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.emoji_emotions_outlined,
+                                          color: os_deep_grey,
+                                        ),
+                                        onPressed: () {
+                                          if (!choosingEmoji) {
+                                            FocusManager.instance.primaryFocus
+                                                .unfocus();
+                                          }
+                                          setState(() {
+                                            choosingEmoji = !choosingEmoji;
+                                          });
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.keyboard_hide_rounded,
+                                          color: os_deep_grey,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            choosingEmoji = false;
+                                          });
+                                          FocusManager.instance.primaryFocus
+                                              .unfocus(); //去除焦点
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              (!isKeyboardVisible && choosingEmoji)
+                                  ? Expanded(child: YourEmoji(
+                                      tap: (res) {
+                                        mixContSectionKey.currentState
+                                            .insertEmoji(res);
+                                        print("$res");
+                                      },
+                                    ))
+                                  : Container(),
+                            ],
+                          ),
+                        ),
+                        bottom: 0,
+                        left: 0,
+                      )
+                    : Container(),
+              ],
+            ),
           ),
         ),
       ),
@@ -63,8 +150,282 @@ class _PostNewMixState extends State<PostNewMix> {
   }
 }
 
-class MixTitileInput extends StatelessWidget {
-  const MixTitileInput({
+enum BodyContType {
+  image,
+  txt,
+}
+
+class BodyCont {
+  BodyContType type; //类型
+  //type==image，cont为path；
+  //type==txt，cont为内容；
+  String cont; //内容
+  BodyCont(BodyContType t, String c) {
+    this.type = t;
+    this.cont = c;
+  }
+}
+
+class MixContSection extends StatefulWidget {
+  MixContSection({Key key}) : super(key: key);
+
+  @override
+  State<MixContSection> createState() => MixContSectionState();
+}
+
+class MixContSectionState extends State<MixContSection> {
+  int hasFocusIndex = -1;
+  int lastEditIndex = 0;
+  int lastEditPos = 0;
+
+  List<BodyCont> body_cont = [
+    BodyCont(BodyContType.txt, ""),
+  ];
+  List<FocusNode> _focusNode = [];
+  List<TextEditingController> _textEditingController = [];
+
+  insertEmoji(String s) {
+    final int lastEditPosTmp = lastEditPos;
+    final String txt = _textEditingController[lastEditIndex].text;
+    _textEditingController[lastEditIndex].text =
+        txt.substring(0, lastEditPos) + s + txt.substring(lastEditPos);
+    setState(() {});
+    lastEditPos = lastEditPosTmp + s.length;
+    setState(() {});
+    unFocus();
+  }
+
+  rebuildControlList() {
+    _focusNode = [];
+    _textEditingController = [];
+    for (var i = 0; i < body_cont.length; i++) {
+      _focusNode.add(FocusNode());
+      _textEditingController.add(
+        TextEditingController(text: body_cont[i].cont),
+      );
+    }
+    unFocus();
+    setState(() {});
+    listenController();
+  }
+
+  unFocus() {
+    FocusManager.instance.primaryFocus.unfocus(); //去除焦点
+  }
+
+  List<Widget> _buildCont() {
+    List<Widget> _body_cont_tmp = [];
+    for (var i = 0; i < body_cont.length; i++) {
+      final BodyCont _body_cont_single = body_cont[i];
+      if (_body_cont_single.type == BodyContType.txt) {
+        _body_cont_tmp.add(MixContInput(
+          controller: _textEditingController[i],
+          focusNode: _focusNode[i],
+        ));
+      }
+      if (_body_cont_single.type == BodyContType.image) {
+        _body_cont_tmp.add(MixPic(
+          path: _body_cont_single.cont,
+          delete: () {
+            for (var i = 0; i < body_cont.length; i++) {
+              if (body_cont[i].cont == _body_cont_single.cont) {
+                if (i + 1 < body_cont.length &&
+                    _textEditingController[i + 1].text == "") {
+                  _textEditingController.removeAt(i);
+                  _textEditingController.removeAt(i);
+                  _focusNode.removeAt(i);
+                  _focusNode.removeAt(i);
+                  body_cont.removeAt(i);
+                  body_cont.removeAt(i);
+                } else {
+                  _textEditingController.removeAt(i);
+                  _focusNode.removeAt(i);
+                  body_cont.removeAt(i);
+                }
+                setState(() {});
+              }
+            }
+            mergeInput();
+          },
+        ));
+      }
+    }
+    return _body_cont_tmp;
+  }
+
+  mergeInput() {
+    for (var i = 0; i < body_cont.length; i++) {
+      if (i < body_cont.length - 1 &&
+          body_cont[i].type == BodyContType.txt &&
+          body_cont[i + 1].type == BodyContType.txt) {
+        //合并两个相邻的Input
+        body_cont[i].cont += "\n" + body_cont[i + 1].cont;
+        _textEditingController[i].text +=
+            "\n" + _textEditingController[i + 1].text;
+        body_cont.removeAt(i + 1);
+        _textEditingController.removeAt(i + 1);
+        _focusNode.removeAt(i + 1);
+        rebuildControlList();
+      }
+    }
+  }
+
+  addPic() async {
+    unFocus();
+    ImagePicker _picker = ImagePicker();
+    List<XFile> _imgs = await _picker.pickMultiImage();
+    for (var i = 0; i < _imgs.length; i++) {
+      final XFile element = _imgs[i];
+      body_cont.add(BodyCont(BodyContType.image, element.path));
+      body_cont.add(BodyCont(BodyContType.txt, ""));
+    }
+    rebuildControlList();
+    setState(() {});
+  }
+
+  listenController() {
+    for (var i = 0; i < _textEditingController.length; i++) {
+      _textEditingController[i].addListener(() {
+        if (body_cont[i].type == BodyContType.txt) {
+          body_cont[i].cont = _textEditingController[i].text;
+          print("${_textEditingController[i].text}");
+        }
+        setState(() {
+          lastEditPos = _textEditingController[i].selection.base.offset;
+        });
+      });
+    }
+    for (var i = 0; i < _focusNode.length; i++) {
+      _focusNode[i].addListener(() {
+        if (_focusNode[i].hasFocus) {
+          setState(() {
+            lastEditIndex = i;
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    listenController();
+    rebuildControlList();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ..._buildCont(),
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: addPic,
+                icon: Icon(Icons.add),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MixPic extends StatefulWidget {
+  String path;
+  Function delete;
+  MixPic({
+    Key key,
+    this.path,
+    this.delete,
+  }) : super(key: key);
+
+  @override
+  State<MixPic> createState() => _MixPicState();
+}
+
+class _MixPicState extends State<MixPic> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Stack(
+        children: [
+          Image.file(File(widget.path)),
+          Positioned(
+            right: 10,
+            top: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0x44000000),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.close,
+                  color: Color(0xccffffff),
+                ),
+                onPressed: () {
+                  if (widget.delete != null) {
+                    widget.delete();
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MixContInput extends StatefulWidget {
+  TextEditingController controller;
+  FocusNode focusNode;
+  String placeholder;
+  MixContInput({
+    Key key,
+    this.controller,
+    this.focusNode,
+    this.placeholder,
+  }) : super(key: key);
+
+  @override
+  State<MixContInput> createState() => _MixContInputState();
+}
+
+class _MixContInputState extends State<MixContInput> {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: TextField(
+          controller: widget.controller,
+          focusNode: widget.focusNode,
+          cursorColor: os_deep_blue,
+          maxLines: null,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 15),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            hintText: widget.placeholder ?? "点击输入内容",
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MixTitleInput extends StatelessWidget {
+  const MixTitleInput({
     Key key,
   }) : super(key: key);
 
